5c633b2c9a7beea88ce43bc65d4798ced613310d
Roberval Filho <rslfilho@gmail.com>
2022-03-21 19:21:45 -0300

    [ PR ] - Adicionando Swagger à aplicação (#6)
    * feat : adicionando middleware com configuração do Swagger
    
    * feat : adicionando arquivo com a descrição Swagger
    
    * feat : adicionando Swagger à pipeline do Express

--- src/api/app.js
+++ src/api/app.js
@@ -2,13 +2,14 @@ const cors = require('cors');
 const express = require('express');
 
 const root = require('../routes/root');
-const { error } = require('../middlewares');
+const { error, swagger } = require('../middlewares');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/swagger', swagger.serve, swagger.setup);
 app.use(root);
 app.use(error);
 
